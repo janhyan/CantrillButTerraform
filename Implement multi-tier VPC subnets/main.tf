00b083337c0df40f6fd69cd8cd5e0fa0145cafa3
hyanjan <hyanxjan@gmail.com>
2024-09-06 18:48:06 +0800

    refactor: Add condition to assign public ipv4 to web subnets

--- Implement multi-tier VPC subnets/main.tf
+++ Implement multi-tier VPC subnets/main.tf
@@ -9,14 +9,17 @@ resource "aws_vpc" "a4l-vpc1" {
   }
 }
   
-resource "aws_subnet" "sn-reserved-A" {
+resource "aws_subnet" "this" {
   for_each = var.subnet_config
 
-  vpc_id = aws_vpc.a4l-vpc1.id
-  cidr_block = each.value.cidr_block
-  assign_ipv6_address_on_creation = true
-  ipv6_cidr_block = cidrsubnet(aws_vpc.a4l-vpc1.ipv6_cidr_block, 8, each.value.ipv6_config)
-  availability_zone = each.value.availability_zone
+  vpc_id                           = aws_vpc.a4l-vpc1.id
+  cidr_block                       = each.value.cidr_block
+  assign_ipv6_address_on_creation  = true
+  ipv6_cidr_block                  = cidrsubnet(aws_vpc.a4l-vpc1.ipv6_cidr_block, 8, each.value.ipv6_config)
+  availability_zone                = each.value.availability_zone
+
+  # If web subnet, assign ipv4
+  map_public_ip_on_launch = contains(["sn-web-A", "sn-web-B", "sn-web-C"], each.value.name) ? true : false
 
   tags = {
     Name = each.value.name
