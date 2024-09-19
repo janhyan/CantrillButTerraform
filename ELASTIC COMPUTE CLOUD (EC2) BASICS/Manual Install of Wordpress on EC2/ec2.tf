c69872b5d5270564df76d411fc4cbe637f4ee2f4
hyanjan <hyanxjan@gmail.com>
2024-09-19 22:39:40 +0800

    feat: Deployment of EC2 machines with proper configuration for Wordpress setup

--- ELASTIC COMPUTE CLOUD (EC2) BASICS/Manual Install of Wordpress on EC2/ec2.tf
+++ ELASTIC COMPUTE CLOUD (EC2) BASICS/Manual Install of Wordpress on EC2/ec2.tf
@@ -8,32 +8,30 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_security_group" "a4l-ec2-sg" {
-    vpc_id = aws_vpc.a4l-vpc1.id
-    description = "Enable SSH access via port 22 IPv4 & v6"
-    ingress {
-      cidr_blocks = [ "0.0.0.0/0" ]
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-    }
-    ingress {
-        cidr_blocks = [ "0.0.0.0/0" ]
-        protocol = "tcp"
-        from_port = 80
-        to_port = 80
-    }
-    ingress {
-        cidr_blocks = ["::/0"]
-        protocol = "tcp"
-        from_port = 22
-        to_port = 22
-    }
-  
+  vpc_id      = aws_vpc.a4l-vpc1.id
+  description = "Enable SSH access via port 22 IPv4 & v6"
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+  }
 }
 
 resource "aws_instance" "a4l-public" {
-  instance_type = "t2.micro"
-    ami = data.aws_ami.ubuntu.id
-    subnet_id = aws_subnet.a4l-sn["sn-web-A"].id
+  instance_type   = "t2.micro"
+  ami             = data.aws_ami.ubuntu.id
+  subnet_id       = aws_subnet.a4l-sn["sn-web-A"].id
+  security_groups = [aws_security_group.a4l-ec2-sg.id]
 
+  tags = {
+    Name = "a4l-publicEC2"
+  }
 }
